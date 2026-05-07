94158cd0f44cd4d81fc075193ee90e0eb48b1164
Aren <Aren.silver12@gmail.com>
2026-05-07 14:05:46 +0330

    Add SolanaProvider component for wallet integration

---
+++ components/SolanaProvider.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { ReactNode, useMemo } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
+import { clusterApiUrl } from "@solana/web3.js";
+
+require("@solana/wallet-adapter-react-ui/styles.css");
+
+export default function SolanaProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const endpoint = clusterApiUrl("devnet");
+
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          {children}
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
